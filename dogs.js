5233b1c52ccaff3d35de51e7e6c3abb22e2f638b
vhuang04 <vanessahuang011304@gmail.com>
2025-04-29 17:46:19 -0400

    edit

--- dogs.js
+++ dogs.js
@@ -1,5 +1,33 @@
+//random dog pic 
+function randomdog(){
+    for(let i = 0; i < 8; i++){
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then((result)=> result.json())
+        .then((resultjson) => {
+            console.log(resultjson);
+
+            const container = document.getElementById("slideshow_container")
+            console.log(container)
+            const slide = document.createElement("div")
+            slide.className = "myslides fade"
+            slide.innerHTML = `<img src="${resultjson.message}" style="width:100%; height:500px;">`;
+            if (i != 0){               
+                 slide.style.display = "none"
+            }
+
+            container.appendChild(slide)
+
+        })
+    }
+}
+
+window.onload = randomdog;
+
+
+
+///////////////////////////////////////////////////////////
 //slideshow
-let slideIndex = 1;
+let slideIndex = 0;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -27,8 +55,10 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-/////////////////////////////////
 
+
+
+/////////////////////////////////
 //api dogs 
 
 function dogsapi(){
